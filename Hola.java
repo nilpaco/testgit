d7039dfd54189b9c8567ee63e1c0ac1da7e4cd10
Nil Panés <nil.panes@gmail.com>
2016-03-02 20:24:56 +0100

    ñadimos una parte

--- Hola.java
+++ Hola.java
@@ -1,5 +1,6 @@
 public class Hola{
+	private static String nombre = "Nil";
 	public static void main(String[] args){
-	System.out.println("Hola que tal como estas"+args[0]);
+	System.out.println("Hola que tal como estas"+nombre);
 }
 }
